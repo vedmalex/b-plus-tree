3d761fda2c895772a8f6db764a59733f38c600bc
Alexey Vedmedenko <vedmalex@gmail.com>
2021-01-26 01:53:28 +0300

    fix issue with find first

--- src/methods/find_first_node.ts
+++ src/methods/find_first_node.ts
@@ -6,13 +6,17 @@ export function find_first_node(tree: BPlusTree, key: ValueType) {
   let cur = tree.root
   while (cur.leaf != true) {
     let i = find_first_pos_to_insert(cur.keys, key)
-    if (cur.keys[i] == key) {
-      // это пограничный элемент, поэтому с тем же номером
-      // в children будут те что меньше ключа
-      i += 1
-    }
-
     cur = cur.children[i]
+    //searchion exact first node!!!
+    if (key <= cur.min && key <= cur.left?.max) {
+      while (key <= cur.left?.max) {
+        cur = cur.left
+      }
+    } else if (cur.max < key) {
+      do {
+        cur = cur.right
+      } while (!(key >= cur.right?.min))
+    }
   }
   return cur
 }
